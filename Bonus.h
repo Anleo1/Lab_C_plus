f3530bf44c9fee19b9fe835d5995bd072cbbc859
Anastasia <95341361+Anleo1@users.noreply.github.com>
2025-05-20 03:53:58 +0300

    Add files via upload

---
+++ Bonus.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "Types.h"
+#include "Constants.h"
+
+class Bonus {
+public:
+    Bonus(float x, float y, BonusType t);
+    void update(float deltaTime);
+    void draw(sf::RenderWindow& window) const;
+    void activate();
+    void deactivate();
+    void use();
+    bool isActive() const;
+    sf::FloatRect getBounds() const;
+    BonusType getType() const;
+    sf::Vector2f getPosition() const;
+
+private:
+    sf::RectangleShape shape;
+    BonusType type;
+    bool active;
+    bool used;
+};
