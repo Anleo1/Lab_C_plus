93d3d4e2881da12f393c5b1a0e04f89d6a69e0aa
Anastasia <95341361+Anleo1@users.noreply.github.com>
2025-12-18 01:43:15 +0300

    Add files via upload

---
+++ CommandEngine/TestFunctions.hpp
@@ -0,0 +1,53 @@
+﻿#pragma once
+#ifndef TESTFUNCTIONS_HPP
+#define TESTFUNCTIONS_HPP
+
+#include "Engine.hpp"
+#include "Wrapper.hpp"
+#include <stdexcept>
+#include <cassert>
+#include <cmath>
+
+class TestClass {
+public:
+    int f3(int arg1, int arg2) {
+        return arg1 + arg2;
+    }
+
+    std::string repeat(std::string s, int n) {
+        std::string result;
+        for (int i = 0; i < n; ++i) result += s;
+        return result;
+    }
+
+    int getSquare(int x) {
+        return x * x;
+    }
+
+    double average(double a, double b) {
+        return (a + b) / 2.0;
+    }
+
+    double multiply(double a, double b) {
+        return a * b;
+    }
+
+    bool yes_no(bool flag) { return flag; }
+
+    void print_greeting(std::string name, int times) {
+        for (int i = 0; i < times; ++i) {
+            std::cout << "Hello, " << name << "!" << std::endl;
+        }
+    }
+    double multiply5(double a, double b, double c, double d, double e) {
+        return a * b * c * d * e;
+    }
+
+   
+};
+
+void runAssignmentTest();
+void runAllTypeTests();
+void testDefaultArguments();
+
+#endif
