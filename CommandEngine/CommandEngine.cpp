3a617bde3c73852d64036c2c4ebf9699276a6d81
Anastasia <95341361+Anleo1@users.noreply.github.com>
2025-12-18 01:45:09 +0300

    Add files via upload

---
+++ CommandEngine/CommandEngine.cpp
@@ -0,0 +1,18 @@
+﻿#include "TestFunctions.hpp"
+#include "locale.h"
+
+// Главная функция
+int main() {
+    setlocale(LC_ALL, "Rus");
+    try {
+        runAssignmentTest();
+        runAllTypeTests();
+        testDefaultArguments();
+
+        return 0;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "\nОшибка: " << e.what() << std::endl;
+        return 1;
+    }
+}
